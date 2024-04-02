e2097161df6a064d150250e28ec447571ce8e151
Piotr Bieszczad <biecho88@gmail.com>
2024-04-02 21:59:17 +0000

    Added smoothies.html and tested connection to the database. All works ok

--- smoothies/views.py
+++ smoothies/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Smoothie
 
 # Create your views here.
+
+def all_smoothies(request):
+    """ A view to show all products, including sorting and search queries """
+
+    smoothies = Smoothie.objects.all()
+
+    context = {
+        'smoothies': smoothies,
+    }
+
+    return render(request, 'smoothies/smoothies.html', context)
